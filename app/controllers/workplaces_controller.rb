347a3bd03d6d2cce29ebb95a8ae7c1f0a9c58761
baptiste mulliez <baptistem59@hotmail.com>
2015-11-10 16:26:41 +0100

    prepared for heroku

--- app/controllers/workplaces_controller.rb
+++ app/controllers/workplaces_controller.rb
@@ -1,2 +1,9 @@
 class WorkplacesController < ApplicationController
+
+  def index
+    @workplaces = Workplace.all
+  end
+  def show
+    @workplaces = Workplace.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109195505) do
+ActiveRecord::Schema.define(version: 20151110090411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,22 @@ ActiveRecord::Schema.define(version: 20151109195505) do
     t.string   "address"
     t.string   "city"
     t.string   "postcode"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
+  add_index "companies", ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true, using: :btree
 
   create_table "desks", force: :cascade do |t|
     t.boolean  "seperate"
@@ -49,9 +62,22 @@ ActiveRecord::Schema.define(version: 20151109195505) do
   add_index "desks", ["workplace_id"], name: "index_desks_on_workplace_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "workplaces", force: :cascade do |t|
     t.string   "address"

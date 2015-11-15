c9190a9afc23ee97a03ad10f3c68ca363ab4bb65
baptiste mulliez <baptistem59@hotmail.com>
2015-11-15 14:55:08 +0100

    geoloc heroku temporary fix

--- app/assets/javascripts/geolocalization.js
+++ app/assets/javascripts/geolocalization.js
@@ -1,6 +1,7 @@
 var myPosition = new Array();
 
 $(document).ready(function() {
+   $("#geoloc").click(function() {
      navigator.geolocation.getCurrentPosition(
              onSuccess,
              onError, {
@@ -9,41 +10,37 @@ $(document).ready(function() {
                  maximumAge: 120000
              }
      );
-   function onSuccess(position) {
-       myPosition[0] = position.coords.latitude;
-       myPosition[1] = position.coords.longitude;
-       console.log(myPosition)
-   }
+     function onSuccess(position) {
+         myPosition[0] = position.coords.latitude;
+         myPosition[1] = position.coords.longitude;
+     }
 
-    function onError(err) {
-       var message;
-       switch (err.code) {
-       case 0:
-         message = 'Unknown error: ' + err.message;
-         break;
-       case 1:
-         message = 'You denied permission to retrieve a position.';
-         break;
-       case 2:
-         message = 'The browser was unable to determine a position: ' + error.message;
-         break;
-       case 3:
-         message = 'The browser timed out before retrieving the position.';
-         break;
-       }
-    }
+      function onError(err) {
+         var message;
+         switch (err.code) {
+         case 0:
+           message = 'Unknown error: ' + err.message;
+           break;
+         case 1:
+           message = 'You denied permission to retrieve a position.';
+           break;
+         case 2:
+           message = 'The browser was unable to determine a position: ' + error.message;
+           break;
+         case 3:
+           message = 'The browser timed out before retrieving the position.';
+           break;
+         }
+      }
 
-   $("#geoloc").click(function() {
-      document.cookie = "lat_lng=" + escape(myPosition);
-      window.location = 'companies';
-
-      // console.log(myPosition)
-      //  $.ajax({
-      //     type: "GET",
-      //     url: 'companies',
-      //     // dataType:"json",
-      //     data: 'lat=' + myPosition[0] +'&lon=' + myPosition[1]
-      //  })
+      console.log(myPosition)
+      $.ajax({
+        type: "GET",
+        url: 'companies',
+        // dataType:"json",
+        data: 'lat=' + myPosition[0] +'&lon=' + myPosition[1]
+      })
+      window.location = 'companies/'+'?lat=' + myPosition[0] +'&lon=' + myPosition[1];
    });
 });
 

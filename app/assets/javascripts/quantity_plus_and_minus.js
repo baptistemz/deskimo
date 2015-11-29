7a5f974f7c3ea985f889abda405cc0b2e379f207
baptiste mulliez <baptistem59@hotmail.com>
2015-11-29 02:47:03 +0100

    intendance de bourré

--- app/assets/javascripts/quantity_plus_and_minus.js
+++ app/assets/javascripts/quantity_plus_and_minus.js
@@ -1,11 +1,11 @@
-$('#minus-button').click(function() {
+$('.minus-button').click(function() {
   var $button = $(this)
   var oldValue = $button.parent().find("#desk_quantity").val();
   var newVal = parseFloat(oldValue) - 1;
   $button.parent().find("#desk_quantity").val(newVal);
 });
 
-$('#plus-button').click(function() {
+$('.plus-button').click(function() {
   var $button = $(this)
   var oldValue = $button.parent().find("#desk_quantity").val();
   var newVal = parseFloat(oldValue) + 1;

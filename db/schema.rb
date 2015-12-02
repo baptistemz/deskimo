ab7a6a60d9384ea15803f94cc89a73b606b61b6a
clement ledey <clement.ledey@gmail.com>
2015-12-02 18:34:50 +0100

    Update migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151202141850) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,12 +50,12 @@ ActiveRecord::Schema.define(version: 20151202141850) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date_time"
+    t.datetime "end_date_time"
     t.integer  "desk_id"
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.datetime "start_date_time"
-    t.datetime "end_date_time"
     t.integer  "time_slot_quantity"
     t.string   "time_slot_type"
   end

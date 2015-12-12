4128a2216dd0da99bf8bde478498673fd3f97739
baptiste mulliez <baptistem59@hotmail.com>
2015-12-12 11:38:05 +0100

    intendance index filters, calendar months now working

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,33 +1,73 @@
-var themonth = 1;
-renderCal(themonth);
+if (typeof themonth === 'undefined'){
+  var themonth = 1;
+  renderCal(themonth);
+};
 
 $('.minusmonth').click(function(){
-  themonth += -1;
-  renderCal(themonth);
+    themonth += -1;
+    renderCal(themonth);
 });
 
+
 $('.addmonth').click(function(){
-  themonth += 1;
-  renderCal(themonth);
+    themonth += 1;
+    renderCal(themonth);
+
 });
+
 
 function renderCal(themonth){
 $('.calendar li').remove();
 $('.calendar ul').append('<li>L</li><li>M</li><li>M</li><li>J</li><li>V</li><li>S</li><li>D</li>');
-var d = new Date(),
-  currentMonth = d.getMonth()+themonth, // get this month
-  days = numDays(currentMonth,d.getYear()), // get number of days in the month
-  fDay = firstDay(currentMonth,d.getYear())-1, // find what day of the week the 1st lands on
-  months = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juiller','Aout','Septembre','Octobre','Novembre','Decembre']; // month names
 
+
+  var d = new Date();
+  var currentMonth = d.getMonth()+themonth; // get this month
+  if(currentMonth > 12){
+    var currentMonth = currentMonth - 12
+  }
+
+  var days = numDays(currentMonth,d.getYear()), // get number of days in the month
+    fDay = firstDay(currentMonth,d.getYear())-1, // find what day of the week the 1st lands on
+    months = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']; // month names
 $('.calendar p.monthname').text(months[currentMonth-1]); // add month name to calendar
+
+
 
 for (var i=0;i<fDay-1;i++) { // place the first day of the month in the correct position
   $('<li>&nbsp;</li>').appendTo('.calendar ul');
 }
 
-for (var i = 1;i<=days;i++) { // write out the days
+var availableDaysArray = new Array();
+$("#booking_start_date option").each(function()
+  {
+    availableDaysArray.push(parseInt($(this).val().substring(8, 10)))
+  }
+);
+firstDayMonth = parseInt($("#booking_start_date option:nth(0)").val().substring(5, 7))
+if ($('.calendar p.monthname').text() == months[firstDayMonth - 1]){
+  $("li:contains("+availableDaysArray[0].toString()+")").addClass('enabled')
+}
+
+
+
+
+for (var i = 1;i<=days;i++) {
   $('<li>'+i+'</li>').appendTo('.calendar ul');
+};
+
+// for (var i = availableDaysArray[0];i++) { // write out the days
+//   if (i === availableDaysArray[0]){
+//     $('<li class = "enabled">'+i+'</li>').appendTo('.calendar ul');
+//   }else if($.inArray(i, availableDaysArray)){
+//     $('<li class = "enabled">'+i+'</li>').appendTo('.calendar ul');
+//   }else{
+//     $('<li>'+i+'</li>').appendTo('.calendar ul');
+//   }
+
+// };
+
+
 }
 
 function firstDay(month,year) {
@@ -58,4 +98,4 @@ $('.calendar li').click(function(){ // toggle selected dates
     }
   }
 });
-}
+

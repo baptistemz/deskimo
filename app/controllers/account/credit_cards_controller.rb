c05724c8c9cc823542c13870d415f65d3b8d153c
baptiste mulliez <baptistem59@hotmail.com>
2015-12-13 20:28:46 +0100

    remove useless controller

--- app/controllers/account/credit_cards_controller.rb
+++ app/controllers/account/credit_cards_controller.rb
@@ -1,1 +1,36 @@
-credit_card_controller.rb
+module Account
+  class CreditCardsController < ApplicationController
+    def index
+      @credit_cards = current_user.credit_cards
+    end
+
+    def new
+      current_user.create_or_update_wallet
+      @last_order = current_user.orders.last
+
+
+      params = {
+        UserId: current_user.mangopay_user_id,
+        Currency: "EUR",
+        CardType: "CB_VISA_MASTERCARD"
+      }
+
+      @card_registration = MangoPay::CardRegistration.create(params)
+    end
+
+    def create
+      @credit_card = current_user.credit_cards.build(token: params[:mangopay_card_id])
+      @credit_card.name = "Credit Card ##{params[:mangopay_card_id]}"
+      @last_order = current_user.orders.last
+      if @credit_card.save
+        if @last_order.cart_status == 'payment'
+          redirect_to new_product_checkout_payment_path(@last_order.product)
+        else
+          redirect_to account_credit_cards_path
+        end
+      else
+        redirect_to new_account_credit_card_path
+      end
+    end
+  end
+end

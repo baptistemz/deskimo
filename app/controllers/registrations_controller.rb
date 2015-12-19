fff7bb44c38c80c2ee12ac523aa90f4c8c06b559
baptiste mulliez <baptistem59@hotmail.com>
2015-12-19 03:24:49 +0100

    names required for companies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def sign_up_params
+    if params[:user_type] != "company"
+      params.require(:user).permit( :email, :password, :password_confirmation)
+    else
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
+
   def after_sign_up_path_for(resource)
     if params[:user_type] == "company"
       new_account_company_path(:process => "signup")

260afdc181ed1aad2081b713382fc43df85b3e7e
baptiste mulliez <baptistem59@hotmail.com>
2015-12-22 16:05:50 +0100

    still fighting

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,14 +4,14 @@ class BookingsController < ApplicationController
   def create
     @booking = current_user.bookings.build(booking_params)
     @booking.desk = Desk.find(params[:desk_id])
-    @next_opening_days = @booking.desk.company.get_next_opening_days_array
+    @next_available_days = @booking.desk.get_next_available_days_array
 
     if @booking.time_slot_type == "1/2 journée"
       @booking.end_date = @booking.start_date
       @booking.amount = @booking.desk.half_day_price
     elsif @booking.time_slot_type == "jour(s)"
-      booking_day_number = @next_opening_days.find_index(@booking.start_date)
-      @booking.end_date = @next_opening_days[booking_day_number + @booking.time_slot_quantity - 1]
+      booking_day_number = @next_available_days.find_index(@booking.start_date)
+      @booking.end_date = @next_available_days[booking_day_number + @booking.time_slot_quantity - 1]
       @booking.amount = @booking.desk.daily_price * @booking.time_slot_quantity
     else
       @booking.end_date = @booking.start_date + (7 * @booking.time_slot_quantity).days
@@ -25,7 +25,6 @@ class BookingsController < ApplicationController
                                             start_date: @booking.start_date,
                                             end_date: @booking.end_date
                                             )
-      raise
       if @unavailability.save
         redirect_to company_desk_booking_confirmation_path(@booking.desk.company, @booking.desk, @booking )
       else

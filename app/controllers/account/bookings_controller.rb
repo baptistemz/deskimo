25ec403ea8464132ff8c1245f80a31d7a62386c5
baptiste mulliez <baptistem59@hotmail.com>
2016-01-08 17:26:46 +0100

    account bookings bug correction 2

--- app/controllers/account/bookings_controller.rb
+++ app/controllers/account/bookings_controller.rb
@@ -4,9 +4,18 @@ module Account
     def index
       @user_bookings = current_user.bookings.where(archived: false)
       if current_user.company
-        @open_space_bookings = current_user.company.desks.where(kind: :open_space).first.bookings
-        @closed_office_bookings = current_user.company.desks.where(kind: :closed_office).first.bookings
-        @meeting_room_bookings = current_user.company.desks.where(kind: :meeting_room).first.bookings
+        @open_space = current_user.company.desks.where(kind: :open_space).first
+        @closed_office = current_user.company.desks.where(kind: :closed_office).first
+        @meeting_room = current_user.company.desks.where(kind: :meeting_room).first
+        if @open_space && @open_space.bookings.any?
+          @open_space_bookings = @open_pace.bookings
+        end
+        if @closed_office && @closed_office.bookings.any?
+          @closed_office_bookings = @closed_office.bookings
+        end
+        if @meeting_room && @meeting_room.bookings.any?
+          @meeting_room_bookings = @meeting_room.bookings
+        end
       end
     end
 

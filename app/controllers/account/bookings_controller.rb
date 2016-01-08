848e927013e99c6ea88078368869e652638ceef5
baptiste mulliez <baptistem59@hotmail.com>
2016-01-08 17:50:51 +0100

    last correction account bookings

--- app/controllers/account/bookings_controller.rb
+++ app/controllers/account/bookings_controller.rb
@@ -7,15 +7,16 @@ module Account
         @open_space = current_user.company.desks.where(kind: :open_space).first
         @closed_office = current_user.company.desks.where(kind: :closed_office).first
         @meeting_room = current_user.company.desks.where(kind: :meeting_room).first
-        if @open_space && @open_space.bookings.any?
-          @open_space_bookings = @open_pace.bookings
+        if @open_space
+          @open_space_bookings = @open_space.bookings.where(archived: false)
         end
-        if @closed_office && @closed_office.bookings.any?
-          @closed_office_bookings = @closed_office.bookings
+        if @closed_office
+          @closed_office_bookings = @closed_office.bookings.where(archived: false)
         end
-        if @meeting_room && @meeting_room.bookings.any?
-          @meeting_room_bookings = @meeting_room.bookings
+        if @meeting_room
+          @meeting_room_bookings = @meeting_room.bookings.where(archived: false)
         end
+
       end
     end
 

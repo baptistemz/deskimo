5302cc4a0e6c11bfa1cdeb48dfdd0ea4ae4e4a6f
baptiste mulliez <baptistem59@hotmail.com>
2016-01-12 18:09:31 +0100

    booking creation js takes in account nav tabs

--- app/assets/javascripts/booking_creation.js
+++ app/assets/javascripts/booking_creation.js
@@ -1,50 +1,25 @@
-$('#booking_time_slot_type').on('change', function(event) {
+$('.time-slot-type').on('change', function(event) {
 
   if ($(this).val() === 'half_day') {
     $('.half-day-select').removeClass('hidden')
-    $('#booking_time_slot_quantity').val(1)
-    $('#time-slot-quantity').addClass('hidden')
+    $('.time-slot-quantity').val(1)
+    $('.time-slot-quantity').addClass('hidden')
   } else {
     $('.half-day-select').addClass('hidden')
-    $('#time-slot-quantity').removeClass('hidden')
+    $('.time-slot-quantity').removeClass('hidden')
   }
 });
 
-// function timeRecap($param) {
-//   $param.on('change', function(event) {
-//     var endingHour = parseInt($('#booking_start_date_time_4i').val()) + parseInt($('#booking_time_slot_quantity').val())
-//     var endingMinutes = parseInt($('#booking_start_date_time_5i').val())
-//     if (endingMinutes === 0) {
-//       $('#time-recap').text(endingHour+'h')
-//     } else {
-//       $('#time-recap').text(endingHour+'h'+endingMinutes)
-//     }
-//   });
-// };
+$(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', function () {
+  $('.time-slot-type').val('day(s)')
+  if($('.time-slot-quantity').is( ":hidden" )){
+    $('.time-slot-quantity').removeClass('hidden')
+  }
+  if($('.half-day-select').is( ":visible" )){
+    $('.half-day-select').addClass('hidden')
+  }
+});
 
 
-// timeRecap($('#booking_start_date_time_4i'));
-// timeRecap($('#booking_start_date_time_5i'));
-// timeRecap($('#booking_time_slot_quantity'));
-
-// $('.plus-button').on('click', function(event) {
-//   var endingHour = parseInt($('#booking_start_date_time_4i').val()) + parseInt($('#booking_time_slot_quantity').val()) + 1
-//   var endingMinutes = parseInt($('#booking_start_date_time_5i').val())
-//   if (endingMinutes === 0) {
-//     $('#time-recap').text(endingHour+'h')
-//   } else {
-//     $('#time-recap').text(endingHour+'h'+endingMinutes)
-//   }
-// });
-
-// $('.minus-button').on('click', function(event) {
-//   var endingHour = parseInt($('#booking_start_date_time_4i').val()) + parseInt($('#booking_time_slot_quantity').val()) - 1
-//   var endingMinutes = parseInt($('#booking_start_date_time_5i').val())
-//   if (endingMinutes === 0) {
-//     $('#time-recap').text(endingHour+'h')
-//   } else {
-//     $('#time-recap').text(endingHour+'h'+endingMinutes)
-//   }
-// });
 
 

0056ac8e1a4846624b2bd10a65fec9cb89011ce6
baptiste mulliez <baptistem59@hotmail.com>
2016-01-13 09:24:01 +0100

    index search filters, location and map solving

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 class CompaniesController < ApplicationController
 
   def index
-    if params[:lat].presence && params[:lng].presence
+    if params[:lat].presence && params[:lng].presence && params[:full_address].presence == false
       @location = [params[:lat], params[:lng]]
     elsif params[:full_address].presence
       @location = Geocoder.coordinates(params[:full_address])

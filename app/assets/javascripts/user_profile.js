cb86ac14e8a19411a5ee4368c0bf425ac7e00143
baptiste mulliez <baptistem59@hotmail.com>
2016-01-24 18:21:49 +0100

    profile update page

---
+++ app/assets/javascripts/user_profile.js
@@ -0,0 +1,15 @@
+$('#link_to_pc').click(function(){
+  $('#edit-personal-infos').toggleClass('hidden');
+  $('#password-change').toggleClass('hidden');
+  $('#link_to_pc').toggleClass('hidden');
+  $('#link_to_pi').toggleClass('hidden');
+});
+
+$('#link_to_pi').click(function(){
+  $('#edit-personal-infos').toggleClass('hidden');
+  $('#password-change').toggleClass('hidden');
+  $('#link_to_pc').toggleClass('hidden');
+  $('#link_to_pi').toggleClass('hidden');
+});
+
+

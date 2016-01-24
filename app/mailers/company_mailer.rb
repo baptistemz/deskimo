50d51fda08712c8743ac777870b159135f436af6
baptiste mulliez <baptistem59@hotmail.com>
2016-01-24 12:53:11 +0100

    about page

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -1,15 +1,15 @@
-class UserMailer < ApplicationMailer
+class CompanyMailer < ApplicationMailer
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.welcome.subject
   #
-  def welcome(user)
-    @user= user
-    mail to: @user.email, subject: t("mailer.welcome")
+  def new(company)
+    @company = company
+    @user = company.user
+    mail to: @user.email, subject:
   end
-
   def booking(booking)
   end
 end

457e157fd141813c2ab19adbe455e59bd12d2666
baptiste mulliez <baptistem59@hotmail.com>
2016-02-09 09:25:05 +0100

    geoloc for itinerary

--- app/assets/javascripts/geolocalization.js
+++ app/assets/javascripts/geolocalization.js
@@ -2,6 +2,7 @@ $("#geoloc").click(geoTag);
 $("#geo-open-space").click(geoTag);
 $("#geo-closed-office").click(geoTag);
 $("#geo-meeting-room").click(geoTag);
+$("#geo-itinerary").click(geoIt);
 
 function geoTag() {
   var myPosition = new Array();
@@ -41,4 +42,46 @@ function geoTag() {
     }
   });
 };
+
+function geoIt() {
+  // var myPosition = new Array();
+
+  $(document).ready(function() {
+    navigator.geolocation.getCurrentPosition(
+            onSuccess,
+            onError, {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 120000
+    });
+    function onSuccess(position) {
+      // myPosition[0] = position.coords.latitude;
+      // myPosition[1] = position.coords.longitude;
+      // document.cookie = "lat_lng=" + escape(myPosition);
+      // window.location = 'companies';
+      // "https://maps.google.com?saddr="+position.coords.latitude+position.coords.longitude+"&daddr=#{@company.address.gsub(/\s+/, "+")},#{@company.postcode},#{@company.city.gsub(/\s+/, "+")}"
+      location.href = "https://www.google.com/maps/dir/"+position.coords.latitude+","+position.coords.longitude+"/"+$('#company_address').text()
+      }
+
+
+    function onError(err) {
+      var message;
+      switch (err.code) {
+        case 0:
+          message = 'Unknown error: ' + err.message;
+          break;
+        case 1:
+          message = 'You denied permission to retrieve a position.' + err.message;
+          break;
+        case 2:
+          message = 'The browser was unable to determine a position: ' + err.message;
+          break;
+        case 3:
+          message = 'The browser timed out before retrieving the position.';
+          break;
+      }
+    }
+  });
+};
+
 

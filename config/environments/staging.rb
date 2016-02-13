78494b725aba1b52fe56518ca0f253a63511a53b
baptiste mulliez <baptistem59@hotmail.com>
2016-02-13 12:54:07 +0100

    restrict access to staging env 3

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 MyApp::Application.configure do
-  config.middleware.use '::Rack::Auth::Basic' do |u, p|
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
     [u, p] == [ENV['MY_SITE_USERNAME'], ENV['MY_SITE_SECRET']]
   end
 

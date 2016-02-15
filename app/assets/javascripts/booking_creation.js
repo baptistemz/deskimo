76805a27d00e78048e73fea6111c074165cebc2f
baptiste mulliez <baptistem59@hotmail.com>
2016-02-15 19:40:45 +0100

    javascript corrections for end date predictions

--- app/assets/javascripts/booking_creation.js
+++ app/assets/javascripts/booking_creation.js
@@ -1,36 +1,38 @@
-$('.time-slot-type').on('change', function(event) {
+if($("#desk-choice").length > 0){
+  $('.time-slot-type').on('change', function(event) {
 
-  if ($(this).val() === 'half_day') {
-    $('.half-day-select').removeClass('hidden')
-    $('.time-slot-quantity').val(1)
-    $('.time-slot-quantity').addClass('hidden')
-  } else {
-    $('.half-day-select').addClass('hidden')
-    $('.time-slot-quantity').removeClass('hidden')
-  }
-});
+    if ($(this).val() === 'half_day') {
+      $('.end_prediction').addClass('hidden')
+      $('.half-day-select').removeClass('hidden')
+      $('.time-slot-quantity').val(1)
+      $('.time-slot-quantity').addClass('hidden')
+    } else {
+      $('.half-day-select').addClass('hidden')
+      $('.time-slot-quantity').removeClass('hidden')
+    }
+  });
 
-$(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', function () {
-  $('.time-slot-type').val('day(s)')
-  if($('.time-slot-quantity').is( ":hidden" )){
-    $('.time-slot-quantity').removeClass('hidden')
-  }
-  if($('.half-day-select').is( ":visible" )){
-    $('.half-day-select').addClass('hidden')
-  }
-});
+  $(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+    $('.time-slot-type').val('day(s)')
+    if($('.time-slot-quantity').is( ":hidden" )){
+      $(this).removeClass('hidden')
+    }
+    if($('.end_prediction').is( ":hidden" )){
+      $(this).removeClass('hidden')
+    }
+    if($('.half-day-select').is( ":visible" )){
+      $(this).addClass('hidden')
+    }
+   kind = e.currentTarget.id.slice(0,-4)
+   $(".predicted_end").empty()
+   giveEndEstimation(kind)
+  });
 
 // $(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', clearEnd)
-if($("#desk-choice").length > 0){
   $(document).ready(function(){
     kind = $("#desk-choice li.active a").attr('id').slice(0,-4)
     giveEndEstimation(kind)
 
-  });
-
-  $(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
-   kind = e.currentTarget.id.slice(0,-4)
-   giveEndEstimation(kind)
   });
 }
 
@@ -44,13 +46,17 @@ function giveEndEstimation(kind) {
 
   function predictEnd() {
     if($("." + kind + "_booking #booking_time_slot_type").val() == 'day(s)'){
-      $("." + kind + "_booking .end_prediction").removeClass('hidden')
+      if ($("." + kind + "_booking .end_prediction").hasClass('hidden')){
+        $("." + kind + "_booking .end_prediction").removeClass('hidden')
+      }
       var nextDays = $("." + kind + "_booking #booking_start_date > option").map(function() { return $(this).val(); });
       var firstDayIndex = $.inArray($("." + kind + "_booking #booking_start_date").val(), nextDays)
       var lastDayIndex = parseInt(firstDayIndex) + parseInt($("." + kind + "_booking #booking_time_slot_quantity").val()) - 1
       var endDay = nextDays.get(lastDayIndex)
     } else if($("." + kind + "_booking #booking_time_slot_type").val() == 'week(s)'){
-      $("." + kind + "_booking .end_prediction").removeClass('hidden')
+      if ($("." + kind + "_booking .end_prediction").hasClass('hidden')){
+        $("." + kind + "_booking .end_prediction").removeClass('hidden')
+      }
       var nextDays = $("." + kind + "_booking #booking_start_date > option").map(function() { return $(this).val(); });
       var firstDayParts = $("." + kind + "_booking #booking_start_date").val().split('/')
       var firstDayDate = new Date(firstDayParts[2], firstDayParts[1] - 1, firstDayParts[0])
@@ -70,6 +76,8 @@ function giveEndEstimation(kind) {
         console.log(fictiveLastDay)
         endDay = "Week(s) booking impossible"
       }
+    } else {
+      $("." + kind + "_booking .end_prediction").addClass('hidden')
     }
 
     function getDateString(date) {

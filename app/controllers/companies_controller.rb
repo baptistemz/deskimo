5e26fbc8d05bc4a26dd23e1061f49e5fa1d9a89c
baptiste mulliez <baptistem59@hotmail.com>
2016-03-26 14:33:38 +0100

    pagination again and again and again........

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -42,7 +42,7 @@ class CompaniesController < ApplicationController
       search_conditions[:kinds] = @kind
     end
 
-    @companies = Company.search('*', where: search_conditions, order: sort_conditions, aggs: aggregations, :page => params[:page], :per_page => 6)
+    @companies = Company.search('*', where: search_conditions, order: sort_conditions, aggs: aggregations, :page => params[:page], :per_page => 2)
     if @companies.empty?
       @empty_message = 'Aucun bureau disponible ne correspond à votre recherche'
     end
